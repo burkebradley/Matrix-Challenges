23da3e5b4293c25062cc35a219a1ab900f326ef8
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-09-27 08:43:47 -0600

    began challenge two

--- 2D Array Challenges/src/MatrixChallenges.java
+++ 2D Array Challenges/src/MatrixChallenges.java
@@ -5,13 +5,31 @@ public class MatrixChallenges
 		public static void main(String[] args)
 			{
 				challengeOne();
+				challengeTwo();
 				
 			}
 		public static void challengeOne()
 		{
 				int numbers [][]= new int [3][4];
-				numbers [0][0] =   
+				numbers [0][0] = 2;
+				numbers [0][1] = 4;
+				numbers [0][2] = 6;
+				numbers [0][3] = 8;
+				numbers [1][0] = 3;
+				numbers [1][1] = 6;
+				numbers [1][2] = 9;
+				numbers [1][3] = 12;
+				numbers [2][0] = 4;
+				numbers [2][1] = 8;
+				numbers [2][2] = 12;
+				numbers [2][3] = 16;
+				
+				System.out.println(numbers [1][2]);
 					
 			}
+		public static void challengeTwo()
+		{
+			s
+		}
 
 	}
